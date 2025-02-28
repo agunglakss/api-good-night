cbad6cde736fdfd3e28496bcdf6ba9ed61c5c4ad
Agung Laksono <agung.laksono@Agungs-MacBook-Pro.local>
2025-02-28 19:56:41 +0700

    feat: implement authorization user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  include ApiResponse
+  before_action :authenticate_api_token!
+  
+  private
+
+  def authenticate_api_token!
+    api_token = request.headers['API-TOKEN']
+    @current_user = User.find_by(api_token: api_token)
+
+    # return unauthorized if api_key does'nt exist
+    error('Unauthorized', :unauthorized) unless @current_user
+  end
 end
