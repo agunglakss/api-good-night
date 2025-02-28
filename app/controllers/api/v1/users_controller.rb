c12a89ccf2f971f0bd1b858e3d0228d84c6d36e3
Agung Laksono <agung.laksono@Agungs-MacBook-Pro.local>
2025-02-28 17:58:15 +0700

    chore: create users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Api::V1::UsersController < ApplicationController
+  # TODO : create user
+  # TODO : show list users
+  # TODO : update user
+  # TODO : delete user
+end
