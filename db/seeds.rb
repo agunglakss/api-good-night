fe0eb93a9b6143aa46d6e4bf63c0c4974ae98fdf
Agung Laksono <agung.laksono@Agungs-MacBook-Pro.local>
2025-02-28 18:03:21 +0700

    chore: config seed for generate users data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+User.destroy_all
+puts "Cleared existing users."
+
+# create 1000 users
+1000.times do
+  User.create!(
+    name: Faker::Name.unique.name,
+    api_token: SecureRandom.hex(20)
+  )
+rescue ActiveRecord::RecordInvalid => e
+  retry # Retry if duplicate name occurs
+end
+
+puts "Seeded #{User.count} users successfully."
